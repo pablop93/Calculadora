a636f21b58e045ff7bfd0b68d880bdc7741196cb
Pablo Perez Barrera <pabloperezbarrera93@gmail.com>
2023-10-18 19:06:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/calculadora/CalculatorOperation.kt
@@ -0,0 +1,8 @@
+package com.example.calculadora
+
+sealed class CalculatorOperation(val symbol: String) {
+    object Add: CalculatorOperation("+")
+    object Subtract: CalculatorOperation("-")
+    object Multiply: CalculatorOperation("x")
+    object Divide: CalculatorOperation("/")
+}
